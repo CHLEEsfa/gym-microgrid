fdae5890d07f1f8892c5d5a6fdd1b48887c99c14
CHLEEsfa <34898732+CHLEEsfa@users.noreply.github.com>
2018-07-01 19:02:15 +0100

    Update __init__.py

--- gym_microgrid/__init__.py
+++ gym_microgrid/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='microgrid-v0',
-    entry_point='gym_microgrid.envs:microgridEnv',
+    entry_point='gym_microgrid.envs:MicroGridEnv',
 )
