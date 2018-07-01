a9072838803cca3417f8fa21e8f614366ae3e52d
CHLEEsfa <34898732+CHLEEsfa@users.noreply.github.com>
2018-07-01 20:10:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gym_microgrid',
+      version='0.0.1',
+      install_requires=['gym',
+                        'pandas',
+                        'math']
+)  
